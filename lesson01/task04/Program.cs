a2d2522c0a5141bd7fc67e6ec13d006ef482dc24
Natalia Moz <moznatali117@gmail.com>
2024-07-07 17:53:40 +0300

    add task3

---
+++ lesson01/task04/Program.cs
@@ -0,0 +1,26 @@
+﻿int a = 5;
+int b = 11;
+int c = 2;
+int d = 6;
+int e = 8;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
+
